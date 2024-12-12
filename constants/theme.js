124a8da26742f6091b6e8040a0b400d05019eedc
shahzain19 <cncdofficer2018@gmail.com>
2024-12-12 16:27:36 +0500

    commet

---
+++ constants/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+    colors : {
+        primary: "#00C26F",
+        priDark: "#00AC62",
+        dark: "3E3E3E",
+        darklig: '#E1E1E1',
+        grey: '#e3e3e3',
+
+        text: "#494949",
+        textl: "#7C7C7C",
+        textd: "#1D1D1D",
+
+        rose: "#ef4444",
+        rosel: "#f87171",
+    },
+    font : {
+        med:'500',
+        semiB:'600',
+        bold:'700',
+        xtrB:'800',
+    }
+}
